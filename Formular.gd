13b68dc5371b7d3d9373819830b23fa98f7f72f9
Jan Zeman <janzeman3@seznam.cz>
2020-03-27 21:36:35 +0100

    Stavy, budovy, refactoring
    Struktury:
    - stav + budouci stav
    - budovy + budouci budovy
    
    Principy:
    - funguje budova pole
    
    Refactoring:
    - stavy vyjmuty do vlastniho souboru
    
    Signed-off-by: janzeman3 <janzeman3@gmail.com>

--- Formular.gd
+++ Formular.gd
@@ -1,30 +1,63 @@
 extends Node
 
+var stav
+var budouciStav
+var budovy
+var budouciBudovy
 
-# Declare member variables here. Examples:
-var nObili = 200
-var nSuroviny = 0
-var nLidi = 100
-var nVodolary = 100
+class stavVect:
+	var Lidi = 0
+	var Obili = 0
+	var Vodolary = 0
+	var Suroviny = 0
+	
+	func _init(inLidi, inObili, inVodolary, inSuroviny):
+		Lidi = inLidi
+		Obili = inObili
+		Vodolary = inVodolary
+		Suroviny = inSuroviny
 
+class budovyVect:
+	var Pole = 0
+	var Lom = 0
+	var Kamenictvi = 0
+	var Hostinec = 0
+	var Kaple = 0
+	var Kostel = 0
+	var Katedrala = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	stavReset()
+	stav = stavVect.new(100, 500, 100, 0)
+	budouciStav = stavVect.new(0, 0, 0, 0)
+	
+	budovy = budovyVect.new()
+	budouciBudovy = budovyVect.new()
+	
+	$Stav.updateData(stav)
+	updateBudouciStav()
 
-func stavReset():
-	nObili = 200
-	nSuroviny = 0
-	nLidi = 100
-	nVodolary = 100
-	stavRefresh()
-
-func stavRefresh():
-	$Stav/Lidi.text = str(nLidi)
-	$Stav/Vodolary.text = str(nVodolary)
-	$Stav/Obili.text = str(nObili)
-	$Stav/Suroviny.text = str(nSuroviny)
+func updateBudouciStav():
+	budouciStav.Lidi = stav.Lidi
+	budouciStav.Obili = stav.Obili
+	budouciStav.Vodolary = stav.Vodolary
+	budouciStav.Suroviny = stav.Suroviny
+	
+	# POLE
+	# vynos: +5 obili
+	budouciStav.Obili += 5 * budouciBudovy.Pole
+	# cena stavby: 20 VD
+	budouciStav.Vodolary -= 20 * (budouciBudovy.Pole-budovy.Pole)
+	
+	#VYZIVA OBYVATEL
+	budouciStav.Obili -= budouciStav.Lidi
+	$BudouciStav.updateData(budouciStav)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _on_Pole_value_changed(value):
+	budouciBudovy.Pole = value
+	print(value)
+	updateBudouciStav()
---
+++ Stav.gd
@@ -0,0 +1,7 @@
+extends GridContainer
+
+func updateData(inStavVect):
+	$Lidi.text = str(inStavVect.Lidi)
+	$Vodolary.text = str(inStavVect.Vodolary)
+	$Obili.text = str(inStavVect.Obili)
+	$Suroviny.text = str(inStavVect.Suroviny)
