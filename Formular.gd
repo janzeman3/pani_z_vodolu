f471741335e8b3980eef8d4ccd44a89df4fb0487
Jan Zeman <janzeman3@seznam.cz>
2020-03-28 20:43:29 +0100

    Refaktoring: Stavby
    Stavby odebrany do vlastniho souboru.
    
    Signed-off-by: janzeman3 <janzeman3@gmail.com>

--- Formular.gd
+++ Formular.gd
@@ -3,8 +3,6 @@ extends Node
 var stav
 var budouciStav
 var delta
-var budovy
-var budouciBudovy
 var tah = 1
 var tahMax = 20
 
@@ -32,125 +30,38 @@ class stavVect:
 		Vodolary = Val1.Vodolary - Val2.Vodolary
 		Suroviny = Val1.Suroviny - Val2.Suroviny
 
-
-
-class budovyVect:
-	var Pole = 0
-	var Lom = 0
-	var Kamenictvi = 0
-	var Hostinec = 0
-	var Kaple = 0
-	var Kostel = 0
-	var Katedrala = 0
-	
-	func copy(inVal):
-		Pole = inVal.Pole
-		Lom = inVal.Lom
-		Kamenictvi = inVal.Kamenictvi
-		Hostinec = inVal.Hostinec
-		Kaple = inVal.Kaple
-		Kostel = inVal.Kostel
-		Katedrala = inVal.Katedrala
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	newGame()
 
 func updateView():
 	$Stav.updateData(stav)
-	updateBudouciStav()
-	updateSkoreATahy()
-
-func updateBudouciStav():
-	budouciStav.copy(stav)
+	$Stavby.updateBudouciStav(stav, budouciStav)
 	
-	# POLE
-	# vynos: +5 obili
-	budouciStav.Obili += 5 * budouciBudovy.Pole
-	# cena stavby: 20 VD
-	budouciStav.Vodolary -= 20 * (budouciBudovy.Pole-budovy.Pole)
-	
-	#LOM
-	# vynos: +5 surovin -1 lidi
-	budouciStav.Suroviny += 5 * budouciBudovy.Lom
-	budouciStav.Lidi -= 1 * budouciBudovy.Lom
-	# cena stavby: 100 VD + 2 lidi
-	budouciStav.Vodolary -= 100 * (budouciBudovy.Lom-budovy.Lom)
-	budouciStav.Lidi -= 2 * (budouciBudovy.Lom-budovy.Lom)
-
-	#KAMENICTVI
-	# vynos: +100 vodolaru -5 surovin
-	budouciStav.Vodolary += 100 * budouciBudovy.Kamenictvi
-	budouciStav.Suroviny -= 5 * budouciBudovy.Kamenictvi
-	# cena stavby: 30 surovin + 1 lidi
-	budouciStav.Suroviny -= 30 * (budouciBudovy.Kamenictvi-budovy.Kamenictvi)
-	budouciStav.Lidi -= 1 * (budouciBudovy.Kamenictvi-budovy.Kamenictvi)
-
-	#HOSTINEC
-	# vynos: +100 vodolaru -5 surovin
-	budouciStav.Vodolary += 10 * budouciBudovy.Hostinec
-	budouciStav.Lidi += 1 * budouciBudovy.Hostinec
-	# cena stavby: 30 surovin + 1 lidi
-	budouciStav.Vodolary -= 100 * (budouciBudovy.Hostinec-budovy.Hostinec)
-	budouciStav.Lidi -= 1 * (budouciBudovy.Hostinec-budovy.Hostinec)
-	
-	#KAPLE
-	# vynos: -20 vodolaru +10 lidí
-	budouciStav.Vodolary -= 20 * budouciBudovy.Kaple
-	budouciStav.Lidi += 10 * budouciBudovy.Kaple
-	# cena stavby: 40 surovin
-	budouciStav.Suroviny -= 40 * (budouciBudovy.Kaple-budovy.Kaple)
-
-	#KOSTEL
-	# vynos: -50 vodolaru +20 lidí
-	budouciStav.Vodolary -= 50 * budouciBudovy.Kostel
-	budouciStav.Lidi += 20 * budouciBudovy.Kostel
-	# cena stavby: 110 surovin + 500VD
-	budouciStav.Suroviny -= 110 * (budouciBudovy.Kostel-budovy.Kostel)
-	budouciStav.Vodolary -= 500 * (budouciBudovy.Kostel-budovy.Kostel)
-
-	#KATEDRALA
-	# vynos: -150 vodolaru +50 lidí
-	budouciStav.Vodolary -= 150 * budouciBudovy.Katedrala
-	budouciStav.Lidi += 50 * budouciBudovy.Katedrala
-	# cena stavby: 230 surovin + 2000VD
-	budouciStav.Suroviny -= 230 * (budouciBudovy.Katedrala-budovy.Katedrala)
-	budouciStav.Vodolary -= 2000 * (budouciBudovy.Katedrala-budovy.Katedrala)
-	
-	#VYZIVA OBYVATEL
-	# pokud neni obili, pomrou lidi
-	if budouciStav.Obili < budouciStav.Lidi:
-		budouciStav.Lidi = budouciStav.Obili
-	budouciStav.Obili -= budouciStav.Lidi
-
-	#DANE
-	budouciStav.Vodolary += budouciStav.Lidi
-
 	$BudouciStav.updateData(budouciStav)
 	delta.fillByDelta(budouciStav,stav)
 	$Delta.updateData(delta)
-	
-	
+
+	updateSkoreATahy()
+
 
 func updateSkoreATahy():
 	var skore = stav.Lidi*100 + stav.Vodolary
 	$Skore.text = "Scóre: " + str(skore)
 	$Tah.text = "Tah: " + str(tah) + " z " + str(tahMax)
 
-func _on_Pole_value_changed(value):
-	budouciBudovy.Pole = $Stavby/Pole.value
-	budouciBudovy.Lom = $Stavby/Lom.value
-	budouciBudovy.Kamenictvi = $Stavby/Kamenictvi.value
-	budouciBudovy.Hostinec = $Stavby/Hostinec.value
-	budouciBudovy.Kaple = $Stavby/Kaple.value
-	budouciBudovy.Kostel = $Stavby/Kostel.value
-	budouciBudovy.Katedrala = $Stavby/Katedrala.value
-	updateBudouciStav()
+func _on_Stavby_BudovyChanged():
+	$Stavby.updateBudouciStav(stav, budouciStav)
+
+	$BudouciStav.updateData(budouciStav)
+	delta.fillByDelta(budouciStav,stav)
+	$Delta.updateData(delta)
+
 
 func _on_Tahni_pressed():
 	tah += 1 
 	stav.copy(budouciStav)
-	budovy.copy(budouciBudovy)
+	$Stavby.novyTah()
 	updateView()
 	if tah > tahMax:
 		endGame()
@@ -161,8 +72,7 @@ func newGame():
 	budouciStav = stavVect.new(0, 0, 0, 0)
 	delta = stavVect.new(0, 0, 0, 0)
 	
-	budovy = budovyVect.new()
-	budouciBudovy = budovyVect.new()
+	$Stavby.novaHra()
 
 	updateView()
 	
---
+++ Stavby.gd
@@ -0,0 +1,110 @@
+extends GridContainer
+
+var budovy
+var budouciBudovy
+
+signal BudovyChanged
+
+class budovyVect:
+	var Pole = 0
+	var Lom = 0
+	var Kamenictvi = 0
+	var Hostinec = 0
+	var Kaple = 0
+	var Kostel = 0
+	var Katedrala = 0
+	
+	func copy(inVal):
+		Pole = inVal.Pole
+		Lom = inVal.Lom
+		Kamenictvi = inVal.Kamenictvi
+		Hostinec = inVal.Hostinec
+		Kaple = inVal.Kaple
+		Kostel = inVal.Kostel
+		Katedrala = inVal.Katedrala
+
+func _on_NejakaBudova_value_changed(value):
+	budouciBudovy.Pole = $Pole.value
+	budouciBudovy.Lom = $Lom.value
+	budouciBudovy.Kamenictvi = $Kamenictvi.value
+	budouciBudovy.Hostinec = $Hostinec.value
+	budouciBudovy.Kaple = $Kaple.value
+	budouciBudovy.Kostel = $Kostel.value
+	budouciBudovy.Katedrala = $Katedrala.value
+	emit_signal("BudovyChanged")
+
+func novaHra():
+	budovy = budovyVect.new()
+	budouciBudovy = budovyVect.new()
+
+func novyTah():
+	budovy.copy(budouciBudovy)
+	emit_signal("BudovyChanged")
+
+func updateBudouciStav(stav, budouciStav):
+	budouciStav.copy(stav)
+	
+	# POLE
+	# vynos: +5 obili
+	budouciStav.Obili += 5 * budouciBudovy.Pole
+	# cena stavby: 20 VD
+	budouciStav.Vodolary -= 20 * (budouciBudovy.Pole-budovy.Pole)
+	
+	#LOM
+	# vynos: +5 surovin -1 lidi
+	budouciStav.Suroviny += 5 * budouciBudovy.Lom
+	budouciStav.Lidi -= 1 * budouciBudovy.Lom
+	# cena stavby: 100 VD + 2 lidi
+	budouciStav.Vodolary -= 100 * (budouciBudovy.Lom-budovy.Lom)
+	budouciStav.Lidi -= 2 * (budouciBudovy.Lom-budovy.Lom)
+
+	#KAMENICTVI
+	# vynos: +100 vodolaru -5 surovin
+	budouciStav.Vodolary += 100 * budouciBudovy.Kamenictvi
+	budouciStav.Suroviny -= 5 * budouciBudovy.Kamenictvi
+	# cena stavby: 30 surovin + 1 lidi
+	budouciStav.Suroviny -= 30 * (budouciBudovy.Kamenictvi-budovy.Kamenictvi)
+	budouciStav.Lidi -= 1 * (budouciBudovy.Kamenictvi-budovy.Kamenictvi)
+
+	#HOSTINEC
+	# vynos: +100 vodolaru -5 surovin
+	budouciStav.Vodolary += 10 * budouciBudovy.Hostinec
+	budouciStav.Lidi += 1 * budouciBudovy.Hostinec
+	# cena stavby: 30 surovin + 1 lidi
+	budouciStav.Vodolary -= 100 * (budouciBudovy.Hostinec-budovy.Hostinec)
+	budouciStav.Lidi -= 1 * (budouciBudovy.Hostinec-budovy.Hostinec)
+	
+	#KAPLE
+	# vynos: -20 vodolaru +10 lidí
+	budouciStav.Vodolary -= 20 * budouciBudovy.Kaple
+	budouciStav.Lidi += 10 * budouciBudovy.Kaple
+	# cena stavby: 40 surovin
+	budouciStav.Suroviny -= 40 * (budouciBudovy.Kaple-budovy.Kaple)
+
+	#KOSTEL
+	# vynos: -50 vodolaru +20 lidí
+	budouciStav.Vodolary -= 50 * budouciBudovy.Kostel
+	budouciStav.Lidi += 20 * budouciBudovy.Kostel
+	# cena stavby: 110 surovin + 500VD
+	budouciStav.Suroviny -= 110 * (budouciBudovy.Kostel-budovy.Kostel)
+	budouciStav.Vodolary -= 500 * (budouciBudovy.Kostel-budovy.Kostel)
+
+	#KATEDRALA
+	# vynos: -150 vodolaru +50 lidí
+	budouciStav.Vodolary -= 150 * budouciBudovy.Katedrala
+	budouciStav.Lidi += 50 * budouciBudovy.Katedrala
+	# cena stavby: 230 surovin + 2000VD
+	budouciStav.Suroviny -= 230 * (budouciBudovy.Katedrala-budovy.Katedrala)
+	budouciStav.Vodolary -= 2000 * (budouciBudovy.Katedrala-budovy.Katedrala)
+	
+	#VYZIVA OBYVATEL
+	# pokud neni obili, pomrou lidi
+	if budouciStav.Obili < budouciStav.Lidi:
+		budouciStav.Lidi = budouciStav.Obili
+	budouciStav.Obili -= budouciStav.Lidi
+
+	#DANE
+	budouciStav.Vodolary += budouciStav.Lidi
+
+func _ready():
+	novaHra()
